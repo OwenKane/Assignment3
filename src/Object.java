c1bb336df5b7a8bf1fe3851676dcde894e6e2d11
OwenKane <owen.a.kane@gmail.com>
2015-04-01 16:04:51 +0100

    Started work on scoring system

--- src/Object.java
+++ src/Object.java
@@ -6,22 +6,31 @@ public class Object {
   PApplet parent;
   float objX;
   float objY;
+  float pointBX;
+  float pointBY;
+  float pointS;
   float sizeR;
   float sizeG;
   float dect;
+  float dect2;
   boolean reset;
+  boolean reset2;
 
   Object(PApplet p) {
     parent = p;
     sizeR = 40;
     sizeG = 80;
+    pointS = 40;
     objX = parent.random(0, parent.width);
     objY = parent.random(0, parent.height);
+    pointBX = parent.random(0, parent.width);
+    pointBY = parent.random(0, parent.height);
   }
 
   void display() {
 	  
 	  sizeR += .10; 
+	  pointS += .10;
 	  parent.fill(255, 0, 0);
 	  parent.ellipse(objX, objY, sizeR, sizeR);
 	  
@@ -35,8 +44,15 @@ public class Object {
 	  parent.fill(0, 255, 0);
 	  parent.ellipse(objX, objY, sizeG, sizeG);
 	  
+	  parent.fill(0, 255, 0);
+	  parent.ellipse(pointBX, pointBY, pointS, pointS);//
+	  
 	  if(sizeR > 100){
 		  reset = true;
+	  }
+	  
+	  if(pointS > 100){
+		  reset2 = true;
 	  }
 	  
 	  if(reset){
@@ -47,15 +63,24 @@ public class Object {
 		  reset = false;
 	  }
 	  
+	  if(reset2){
+		  pointBX = parent.random(0, parent.width);
+		  pointBY = parent.random(0, parent.height);
+		  pointS = pointS;
+		  reset2 = false;
+	  }
   }
   
   void dection(){
 	  dect = parent.dist(Main.ballX, Main.ballY, objX, objY);
-	  
 	  if(dect < sizeR && sizeG < 5){
 		  reset = true;
 	  }
 	  
+	  dect2 = parent.dist(Main.ballX, Main.ballY, pointBX, pointBY);	  
+	  if(dect2 < pointS){
+		  reset2 = true;
+	  }
+	  
   }
-
 }
